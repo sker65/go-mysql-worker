2e45ae633726e055e4456a69115ddb05aa8c9ca8
Stefan Rinke <sker65@gmail.com>
2024-03-15 16:14:44 +0100

    - changed logging - added tests - added doc comments

--- export_test.go
+++ export_test.go
@@ -1,3 +1,7 @@
 package main
 
+// this is just for exporting private functions to make them available for testing
+
+// for a generic function we need to provide an instance of the function (here string)
 var StringToAnyList = toAnyList[string]
+var GenerateQuestionsMark = generateQuestionsMark
